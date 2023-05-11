715205de5e9458e81f38a31e7f7e002a7cca628b
Jahangir Hossain <jahangir758505@gmail.com>
2023-05-12 00:10:59 +0600

    Update

--- src/components/dummy-data/data.js
+++ src/components/dummy-data/data.js
@@ -1,12 +1,12 @@
 export const profile_data = {
   skills: [
-    { name: "HTML", value: 70 },
-    { name: "CSS", value: 70 },
-    { name: "JS", value: 70 },
-    { name: "PHP", value: 70 },
-    { name: "WordPress", value: 70 },
-    { name: "React", value: 70 },
-    { name: "Laravel", value: 70 },
+    { name: "HTML", value: 90 },
+    { name: "CSS", value: 74 },
+    { name: "JS", value: 75 },
+    { name: "PHP", value: 80 },
+    { name: "WordPress", value: 60 },
+    { name: "React", value: 75 },
+    { name: "Laravel", value: 65 },
   ],
 
   languages: [
--- src/containers/service.jsx
+++ src/containers/service.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { ServiceCard } from "../components/service-card/ServiceCard";
 import CodeIcon from "@mui/icons-material/Code";
 import { Box, Grid, Typography } from "@mui/material";
+import PhpIcon from "@mui/icons-material/Php";
+import LaptopChromebookIcon from '@mui/icons-material/LaptopChromebook';
 
 export const Service = () => {
   return (
-    <Box sx={{ textAlign: "center", marginTop: "70px" }}>
+    <Box sx={{ textAlign: "center", marginTop: "50px" }}>
       <Typography sx={{ fontSize: 30 }}>My Service</Typography>
       <Typography sx={{ width: "60%", margin: "0 auto" }}>
         Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
@@ -15,13 +17,13 @@ export const Service = () => {
         <ServiceCard
           title="WordPress Development"
           subTitle={"Blog, E-commerce"}
-          icon={<CodeIcon />}
+          icon={<LaptopChromebookIcon />}
           col={4}
         />
         <ServiceCard
           title="Plugin Development"
           subTitle={"Blog, E-commerce"}
-          icon={<CodeIcon />}
+          icon={<LaptopChromebookIcon />}
           col={4}
         />
         <ServiceCard
@@ -33,15 +35,61 @@ export const Service = () => {
         <ServiceCard
           title="Back-End Development"
           subTitle={"Blog, E-commerce"}
-          icon={<CodeIcon />}
+          icon={<LaptopChromebookIcon />}
           col={4}
         />
         <ServiceCard
           title="Web Development"
           subTitle={"Blog, E-commerce"}
-          icon={<CodeIcon />}
+          icon={<PhpIcon />}
           col={4}
         />
+        <Grid item md={4}>
+          <Box
+            sx={{
+              background: "#ffffff",
+              minHeight: 225,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: "30px",
+              "& svg": { color: "#002f63", width: 60, height: 60 },
+            }}
+          >
+            <Typography
+              sx={{
+                color: "#2B2B2B",
+                fontWeight: 500,
+                fontSize: 18,
+                lineHeight: "22px",
+              }}
+            >
+              Advertising
+            </Typography>
+            <Typography
+              sx={{
+                fontSize: 15,
+                color: "#767676",
+                lineHeight: "24px",
+                fontWeight: 400,
+              }}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae
+              nulla diam in ac dictum a urna viverra morbi.
+            </Typography>
+            <Typography
+              sx={{
+                background: "#002f63",
+                padding: "3px 5px",
+                fontSize: 14,
+                margin: "5px 0",
+              }}
+            >
+              Order Now
+            </Typography>
+          </Box>
+        </Grid>
       </Grid>
     </Box>
   );
